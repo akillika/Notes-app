52df24cf54d30a7d3d34889102d8629f83dc1d95
akils <akillika49@gmail.com>
2021-09-14 21:42:34 +0530

    Login and Home Page UI

---
+++ lib/homepage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'dart:math';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Icon(
+              Icons.exit_to_app_outlined,
+              color: Colors.green,
+            ),
+          )
+        ],
+        backgroundColor: Colors.white,
+        title: Text(
+          "All notes",
+          style: TextStyle(color: Colors.green),
+        ),
+      ),
+      body: Container(
+        child: StaggeredGridView.countBuilder(
+          crossAxisCount: 4,
+          itemCount: 8,
+          itemBuilder: (BuildContext context, int index) => new Container(
+              color: Colors.green,
+              child: new Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      new Text(
+                        'Hello world to the maximum',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      new Text('This is a test note'),
+                    ],
+                  ),
+                ),
+              )),
+          staggeredTileBuilder: (int index) =>
+              new StaggeredTile.count(2, index.isEven ? 2 : 1),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
-import 'package:notes_app/homepage.dart';
-import 'package:notes_app/signup.dart';
+import 'package:notes_app/login.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
         elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -25,7 +23,7 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Hello there!",
+              "Lets get started!",
               style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
             SizedBox(
@@ -43,27 +41,28 @@ class _LoginPageState extends State<LoginPage> {
                   border: OutlineInputBorder(), hintText: 'Password'),
             ),
             SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Confirm password'),
+            ),
+            SizedBox(
               height: 30,
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomePage()));
-              },
-              child: Container(
-                height: 50,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    // border: Border.all(),
-                    borderRadius: BorderRadius.all(Radius.circular(100))),
-                child: Center(
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(
-                        color: Colors.white,
-                        // fontWeight: FontWeight.bold,
-                        fontSize: 20),
-                  ),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  // border: Border.all(),
+                  borderRadius: BorderRadius.all(Radius.circular(100))),
+              child: Center(
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      color: Colors.white,
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 20),
                 ),
               ),
             ),
@@ -77,14 +76,14 @@ class _LoginPageState extends State<LoginPage> {
             GestureDetector(
               onTap: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()));
+                    MaterialPageRoute(builder: (context) => LoginPage()));
               },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Dont have an account?"),
+                  Text("Have an account already?"),
                   Text(
-                    " Sign Up",
+                    " Sign In",
                     style: TextStyle(
                         color: Colors.green, fontWeight: FontWeight.bold),
                   ),
